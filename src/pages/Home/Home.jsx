34f875a792039cf01b5bf7ed1c54c7a53da4555a
drewherald <drewherald9@gmail.com>
2023-12-19 19:13:27 -0500

    added loading screen

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,14 +2,26 @@ import React from 'react'
 import TopBar from './TopBar'
 import Featured from './Featured'
 import '../../assets/styles/Home.css'
+import daisyGif from '../../assets/daisy.gif'
+import { useState } from 'react'
 
 export default function Home() {
-  return (
-    <>
-        <div className='mainContainer'>
-            <TopBar className="topbar" />
-            <Featured />
-        </div>
-    </>
-  )
-}
+
+    const [load, setLoad] = useState(true)
+    const f = false
+    setTimeout(() => setLoad(f), 5000)
+ 
+        return (
+            <>
+                <div className='mainContainer'>
+                    <TopBar className="topbar" />
+                    <Featured />
+                </div>
+                <div id='loading' className={`${load ? "" : "none"}`}>
+                    <img src={daisyGif} alt="" />
+                </div>
+                
+            </>
+        )
+    }    
+  
