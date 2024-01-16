8638d627d03c145103ef4ba7faadec532dce6515
drewherald <drewherald9@gmail.com>
2024-01-16 08:47:42 -0600

    uodated Cart total with useMemo for optimization

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,15 +1,26 @@
-import React, { useContext } from 'react'
+import React, { useContext, useMemo } from 'react'
 import getShopItems from '../../utils/getShopItems'
 import TopBar from '../Home/TopBar';
 import DaisyFooter from '../Home/DaisyFooter';
 import '../../assets/styles/Cart/Cart.css'
 import CartItem from './CartItem';
 import { ShopContext } from '../../App'
+import { v4 as uuidv4 } from 'uuid';
 
 export default function Cart() {
 
     const {shopItems, error, loading} = getShopItems(16)
     const globalShop = useContext(ShopContext)
+
+    const total = useMemo(
+       () => {
+        let tempTotal = 0
+        globalShop.cart.forEach((cartItem) => tempTotal += cartItem.price)
+        return tempTotal
+    }
+    )
+   
+    console.log(total)
 
     if (error) return <p>A network error was encountered</p>;
     if (loading) return (
@@ -43,8 +54,11 @@ export default function Cart() {
         </div>
         <div className='cartItems'>
             {globalShop.cart.map((cartItm) => 
-                <CartItem title = {cartItm.title} price={cartItm.price} img={cartItm.image} id={cartItm.id}/>
+                <CartItem title = {cartItm.title} price={cartItm.price} img={cartItm.image} id={cartItm.id} key={uuidv4()}/>
             )}
+            <div>
+                <p className='subTotal'>Subtotal: ${(Math.round(total * 100) / 100).toFixed(2)}</p>
+            </div>
         </div>
        
         
